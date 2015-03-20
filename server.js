18863d1448a30b4b56e2083961d60e07aac4ee96
Jacob Hands <jacob@jacobhands.com>
2015-03-20 15:07:48 -0500

    Changed it so that there are 2 url parameters.
    http://localhost:8090/domain/shortname
    This way you can do an nginx proxy to localhost:8090/domain and multiple
    domains can easily be used from a single instance.

--- server.js
+++ server.js
@@ -7,33 +7,51 @@
  * Created: 6/5/2012
  */
 
-var http = require('http');
-
 var accesslog = require('access-log');
 var easyreq = require('easyreq');
-
+var http = require('http');
 var config = require(process.argv[2] || './config.json');
 
 // Create the server
 http.createServer(function (req, res) {
-  easyreq(req, res);
-  accesslog(req, res);
+    easyreq(req, res);
+    accesslog(req, res);
 
-  var key = req.url.slice(1);
-  if (req.url === '/') {
-    // index
-    res.json(config.urls);
-  } else if (config.urls.hasOwnProperty(key)) {
-    // redirect
-    res.redirect(config.urls[key]);
-  } else {
-    // not found
-    res.notfound();
-  }
-}).listen(config.port, config.host, function() {
-  // Server running
-  console.log('server listening on http://%s:%d', config.host, config.port);
-  console.dir(config.urls);
-  if (config.gid) process.setgid(config.gid);
-  if (config.uid) process.setuid(config.uid);
+    var key = req.url.slice(1).split('/', 2);
+    var reqDomain = key[0];
+    var reqKey = key[1];
+
+    console.log('key=' + key + '=');
+    console.log('reqDomain=' + reqDomain + '=');
+    console.log('reqUrl=' + reqKey + '=');
+
+    if (req.url === '/') {
+        res.setHeader('Content-Type', 'application/json');
+        // Prettier than res.json() when using sub-objects.
+        res.end(JSON.stringify(config.domains, null, 3));
+    }
+    // Check for domain
+    else if (config.domains.hasOwnProperty(reqDomain)) {
+        if (!reqKey) { // No key? Default to index
+            reqKey = 'index';
+        }
+        // Check for subsite page key
+        if (config.domains[reqDomain].hasOwnProperty(reqKey)) {
+            res.redirect(config.domains[reqDomain][reqKey]);
+        }
+        else {
+            // Print out some info to help debug with end users.
+            res.end('ERROR: NO PAGE NAMED =' + reqKey + '= IN DOMAIN =' + reqDomain + '=', 404);
+        }
+    }
+    else {
+        // Print out some info to help debug with end users.
+        res.end('ERROR: NO SUCH DOMAIN =' + reqDomain + '=', 404);
+    }
+}).listen(config.port, config.host, function () {
+    // Server running
+    console.log('server listening on http://%s:%d', config.host, config.port);
+    console.dir(config.domains);
+    if (config.gid) process.setgid(config.gid);
+    if (config.uid) process.setuid(config.uid);
 });
