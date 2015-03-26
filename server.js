299f64221e64f6906018ff1464179552805652f0
Jacob Hands <jacob@jacobhands.com>
2015-03-26 18:27:47 -0500

    Switched to ExpressJS and greatly improved code

--- server.js
+++ server.js
@@ -1,77 +1,40 @@
-#!/usr/bin/env node
-/**
- * Create a simple url-shortener webserver
- *
- * Author: Dave Eddy <dave@daveeddy.com>
- * License: MIT
- * Created: 6/5/2012
- */
-
-var accesslog = require('access-log');
-var easyreq = require('easyreq');
-var http = require('http');
+var express = require('express');
+var urlMapper = require("./urlMapper");
+var app = express();
 var config = require(process.argv[2] || './config.json');
 
-// Create the server
-http.createServer(function (req, res) {
-    easyreq(req, res);
-    accesslog(req, res);
-    console.log("Host: " + req.host);
-    console.log("Hostname: " + req.hostname);
-    var key = req.url.slice(1).split('/');
-    var reqDomain = key[0];
-    var reqKey = key.join('/').substr(reqDomain.length + 1).replace(/(\/$|\/\/$|\/\/\/$)/, '');
-    /*
-    console.log('key=' + key + '=');
-    console.log('reqDomain=' + reqDomain + '=');
-    console.log('reqUrl=' + reqKey + '=');
-    */
-    if (req.url === '/') {
-        res.setHeader('Content-Type', 'application/json');
-        // Prettier than res.json() when using sub-objects.
-        res.end(JSON.stringify(config.domains, null, 3));
-    }
-    // Check for domain
-    else if (config.domains.hasOwnProperty(reqDomain)) {
-        if (!reqKey) { // No key? Default to index
-            reqKey = 'index';
-        }
+// Add url
+app.get('/add', function (req, res) {
+    // TODO
+    res.send("add");
+});
+// Delete url
+app.get('/del', function (req, res) {
+    // TODO
+    res.send("del");
+});
+// List urls
+app.get('/urllist', function (req, res) {
+    sendUrls(res);
+});
+// Route all others to urlMapper
+app.get('/*', function (req, res) {
+    urlMapper.map(req, res)
+});
+// Route empty to mapper (for index pages)
+app.get('/', function (req, res) {
+    urlMapper.map(req, res)
+});
 
-        // Check for subsite page key
-        if (config.domains[reqDomain].hasOwnProperty(reqKey)) {
-            var target = config.domains[reqDomain][reqKey];
-            // see if target is an alias
-            if ((/^alias:/).test(target)) {
-                var aliasKey = target.replace(/^alias:/, '');
-                console.log('aliasKey=' + aliasKey);
-                if (config.domains[reqDomain].hasOwnProperty(aliasKey)) {
-                    target = config.domains[reqDomain][aliasKey];
-                    // Redirect alias
-                    res.redirect(target);
-                }
-                else {
-                    // Alias has no valid target
-                    res.end('ERROR: NO PAGE FROM ALIAS NAMED =' + reqKey + '= IN DOMAIN =' + reqDomain + '=', 404);
-                }
-            }
-            else {
-                // Redirect valid non-alias
-                res.redirect(target);
-            }
-        }
-        else {
-            // Print out some info to help debug with end users.
-            res.end('ERROR: NO PAGE NAMED =' + reqKey + '= IN DOMAIN =' + reqDomain + '=', 404);
-        }
-    }
-    else {
-        // Print out some info to help debug with end users.
-        res.end('ERROR: NO SUCH DOMAIN =' + reqDomain + '=', 404);
-    }
-}).listen(config.port, config.host, function () {
+// Print out urls
+function sendUrls(res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(config.urls, null, 3));
+}
+app.listen(config.port, config.host, function () {
     // Server running
     console.log('server listening on http://%s:%d', config.host, config.port);
-    console.dir(config.domains);
+    console.dir(config.urls);
     if (config.gid) process.setgid(config.gid);
     if (config.uid) process.setuid(config.uid);
 });
--- urlMapper.js
+++ urlMapper.js
@@ -1,34 +1,83 @@
 var config = require(process.argv[2] || './config.json');
 
-exports.map = function (req, res) {
+exports.map = function(req, res) {
     var matchFound = false;
 
     // Get the requested url without any forward slashes at the end.
-    var key = req.params[0].replace(/(\/$|\/\/$|\/\/\/$)/, '');
+
+    var key = req.params[0];
     var host = req.hostname;
-    if(keyExists(host, key)) {
-
+    var defaultKey = 'index';
+    var defaultHost = 'default';
+    var url;
+    var useDefaultKey = false;
+    if (config.hasOwnProperty('default-key-on-fail')) {
+        useDefaultKey = config['default-key-on-fail'];
     }
-
-    return matchFound;
+    var useDefaultHost = false;
+    if (config.hasOwnProperty('default-host-on-fail')) {
+        useDefaultHost = config['default-host-on-fail'];
+    }
+    var useDefaultForEmpty = false;
+    if (config.hasOwnProperty('default-on-empty-request')) {
+        useDefaultForEmpty = config['default-on-empty-request'];
+    }
+    // Check for null key
+    if (key) {
+        key = key.replace(/(\/$|\/\/$|\/\/\/$)/, ''); // remove trailing '/'
+    } else {
+        key = defaultKey;
+        useDefaultKey = useDefaultForEmpty;
+    }
+    // Check key as requested
+    if (keyExists(host, key)) {
+        url = getUrl(host, key);
+        res.redirect(url);
+    }
+    // Try for requested key on default host
+    else if (keyExists(defaultHost, key) && useDefaultHost) {
+        url = getUrl(defaultHost, key);
+        res.redirect(url);
+    }
+    // Try for default key on requested host
+    else if (keyExists(host, defaultKey) && useDefaultKey) {
+        url = getUrl(host, defaultKey);
+        res.redirect(url);
+    }
+    // Try for default key on default host
+    else if (keyExists(defaultHost, defaultKey) && useDefaultHost && useDefaultKey) {
+        url = getUrl(defaultHost, defaultKey);
+        res.redirect(url);
+    }
+    // Respond with error message
+    else {
+        res.end('Error, can not find url: ' + url + ' on host: ' + host);
+    }
 };
-
 // Check hostname exists
 function hostExists(host) {
     var found = false;
-    if(config.urls.hasOwnProperty(host)) {
+    if (config.urls.hasOwnProperty(host)) {
         found = true;
+    } else {
+        throw('Error, no such host: ' + host);
     }
     return found;
 }
+
 // Check if url key exists
 function keyExists(host, key) {
     var found = false;
-    if(hostExists(host) && config[domain].hasOwnProperty(key)) {
-        found = true;
+    if (hostExists(host)) {
+        if (config.urls[host].hasOwnProperty(key)) {
+            found = true;
+        }
+    } else {
+        throw('Error, no such key: ' + key);
     }
     return found;
 }
+
 // Check if a url is an alias rather than an actual url
 function isAlias(url) {
     var alias = false;
@@ -36,4 +85,28 @@ function isAlias(url) {
         alias = true;
     }
     return alias;
+}
+
+// Get the value of a key
+function getRawUrl(host, key) {
+    var urlVal = '';
+    if (keyExists(host, key)) {
+        urlVal = config.urls[host][key];
+    }
+    return urlVal;
+}
+
+// Strip alias of prefix
+function stripAlias(aliasKey) {
+    return aliasKey.replace(/^alias:/, '');
+}
+
+// Get url (resolves aliases)
+function getUrl(host, key) {
+    var url = getRawUrl(host, key);
+    if (isAlias(url)) {
+        var aliasKey = stripAlias(url);
+        url = getRawUrl(host, aliasKey);
+    }
+    return url;
 }
